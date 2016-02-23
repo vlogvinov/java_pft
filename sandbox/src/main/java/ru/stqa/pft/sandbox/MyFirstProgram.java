199f81070d69c96b0cc400bee95a408ba1ec486f
Vladimir Logvinov <volodymyr.logvinov@gmail.com>
2016-02-23 16:02:22 +0200

    Отформатировал

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,7 +1,7 @@
 package ru.stqa.pft.sandbox;
 
-class MyFirstProgram{
-	public static void main(String[] args){
-		System.out.println("Hello, world!");
-	}
+class MyFirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
 }
