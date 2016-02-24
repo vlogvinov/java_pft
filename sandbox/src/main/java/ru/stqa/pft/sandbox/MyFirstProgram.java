8a50477daa54880e3130c91f6f008634f525afb1
Vladimir Logvinov <volodymyr.logvinov@gmail.com>
2016-02-24 16:10:18 +0200

    Classes and Objects

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,19 +3,12 @@ package ru.stqa.pft.sandbox;
 class MyFirstProgram {
     public static void main(String[] args) {
 
-        double length = 5;
-        System.out.println("Площадь квадрата со стороной " + length + " = " + area(length));
+        Square s = new Square(5);
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
-        double a = 4;
-        double b = 6;
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b  + " = " + area(a, b));
-    }
-    
-    public static double area(double l) {
-        return l * l;
-    }
 
-    public static double area(double a, double b) {
-        return a * b;
+        Rectangle r = new Rectangle(4, 6);
+
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
     }
 }
