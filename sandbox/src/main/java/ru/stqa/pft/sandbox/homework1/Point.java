57dce984ad9491182166ad933a87fd5458d200ae
Vladimir Logvinov <volodymyr.logvinov@gmail.com>
2016-02-24 16:51:39 +0200

    Homework 1.4

--- sandbox/src/main/java/ru/stqa/pft/sandbox/homework1/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/homework1/Point.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.sandbox.homework1;
 
+import static java.lang.Math.pow;
+
 public class Point {
     public double a;
     public double b;
@@ -7,5 +9,9 @@ public class Point {
     public Point(double a, double b){
         this.a = a;
         this.b = b;
+    }
+
+    public double distance(Point p) {
+        return  Math.sqrt(pow((this.a - p.a), 2) + (pow((this.b - p.b), 2)));
     }
 }
