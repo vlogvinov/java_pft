c499dba61200f8f8520801fc315e65016a1ed1e1
Vladimir Logvinov <volodymyr.logvinov@gmail.com>
2016-02-28 23:46:49 +0200

    Added SquareTests

---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -0,0 +1,13 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SquareTests {
+    @Test
+    public void testArea(){
+        Square square = new Square(5);
+        Assert.assertEquals(square.area(), 25.0);
+
+    }
+}
