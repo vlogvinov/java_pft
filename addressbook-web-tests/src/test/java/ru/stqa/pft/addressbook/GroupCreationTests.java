ac6a94691c3c10facf170dde1b5bf7bf2bff6a0c
Vladimir Logvinov <volodymyr.logvinov@gmail.com>
2016-02-29 16:12:58 +0200

    extracted group's parameters

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -24,7 +24,7 @@ public class GroupCreationTests {
     public void testGroupCreation() {
         goToGroupsPage();
         initGroupCreation();
-        fillGroupForm();
+        fillGroupForm("my group", "group header", "group footer");
         submitGroupCreation();
         returnToGroupsPage();
         logout();
@@ -55,16 +55,16 @@ public class GroupCreationTests {
         wd.findElement(By.name("new")).click();
     }
 
-    private void fillGroupForm() {
+    private void fillGroupForm(String name, String header, String footer) {
         wd.findElement(By.name("group_name")).click();
         wd.findElement(By.name("group_name")).clear();
-        wd.findElement(By.name("group_name")).sendKeys("my group");
+        wd.findElement(By.name("group_name")).sendKeys(name);
         wd.findElement(By.name("group_header")).click();
         wd.findElement(By.name("group_header")).clear();
-        wd.findElement(By.name("group_header")).sendKeys("group header");
+        wd.findElement(By.name("group_header")).sendKeys(header);
         wd.findElement(By.name("group_footer")).click();
         wd.findElement(By.name("group_footer")).clear();
-        wd.findElement(By.name("group_footer")).sendKeys("group footer");
+        wd.findElement(By.name("group_footer")).sendKeys(footer);
     }
 
     private void submitGroupCreation() {
