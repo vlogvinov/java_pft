8da587670313de8a5afc15506210fbdca0dd0311
Vladimir Logvinov <volodymyr.logvinov@gmail.com>
2016-03-02 16:45:08 +0200

    Created ContactData class

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -23,8 +23,8 @@ public class ContactCreationTests {
     @Test
     public void ContactCreationTests() {
         initContactCreation();
-        fillContactForm("Vladimir", "Vitalievich", "Logvinov", "vlogvinov", "Mr.", "P4", "Zakrevskogo street 67", "5469878", "0639876756", "0994565678", "Fax", "volodymyr.logvinov@gmail.com", "my_second_email@gmail.com", "my_thirdemail@gmail.com", "myhomepage.com", "4", "4", "1993", "3", "2", "2003",
-                "Dmitria Lucenka 13", "5679867", "My super notes");
+        fillContactForm(
+                new ContactData("Vladimir", "Vitalievich", "Logvinov", "vlogvinov", "Mr.", "P4", "Zakrevskogo street 67", "5469878", "0639876756", "0994565678", "Fax", "volodymyr.logvinov@gmail.com", "my_second_email@gmail.com", "my_thirdemail@gmail.com", "myhomepage.com", "4", "4", "1993", "3", "2", "2003", "Dmitria Lucenka 13", "5679867", "My super notes"));
         submitContactCreation();
         returnToHomePage();
     }
@@ -52,90 +52,88 @@ public class ContactCreationTests {
         browser.findElement(By.linkText("add new")).click();
     }
 
-    private void fillContactForm(String firstName, String middleName, String lastName, String nickName, String title, String company, String firstAddress, String homePhoneNumber, String mobilePhoneNumber,
-                                 String workPhoneNumber, String fax, String mainEmail, String secondEmail, String thirdEmail, String homePage, String birthDay, String birthMonth, String birthYear,
-                                 String anniverDay, String anniverMonth, String anniverYear, String secondAddress, String secondHomePhoneNumber, String notes) {
+    private void fillContactForm(ContactData contact) {
         browser.findElement(By.name("firstname")).click();
         browser.findElement(By.name("firstname")).clear();
-        browser.findElement(By.name("firstname")).sendKeys(firstName);
+        browser.findElement(By.name("firstname")).sendKeys(contact.getFirstName());
         browser.findElement(By.name("middlename")).click();
         browser.findElement(By.name("middlename")).clear();
-        browser.findElement(By.name("middlename")).sendKeys(middleName);
+        browser.findElement(By.name("middlename")).sendKeys(contact.getMiddleName());
         browser.findElement(By.name("theform")).click();
         browser.findElement(By.name("lastname")).click();
         browser.findElement(By.name("lastname")).clear();
-        browser.findElement(By.name("lastname")).sendKeys(lastName);
+        browser.findElement(By.name("lastname")).sendKeys(contact.getLastName());
         browser.findElement(By.name("nickname")).click();
         browser.findElement(By.name("nickname")).clear();
-        browser.findElement(By.name("nickname")).sendKeys(nickName);
+        browser.findElement(By.name("nickname")).sendKeys(contact.getNickName());
         /* photo field */
         browser.findElement(By.name("title")).click();
         browser.findElement(By.name("title")).clear();
-        browser.findElement(By.name("title")).sendKeys(title);
+        browser.findElement(By.name("title")).sendKeys(contact.getTitle());
         browser.findElement(By.name("company")).click();
         browser.findElement(By.name("company")).clear();
-        browser.findElement(By.name("company")).sendKeys(company);
+        browser.findElement(By.name("company")).sendKeys(contact.getCompany());
         browser.findElement(By.name("address")).click();
         browser.findElement(By.name("address")).clear();
-        browser.findElement(By.name("address")).sendKeys(firstAddress);
+        browser.findElement(By.name("address")).sendKeys(contact.getFirstAddress());
         browser.findElement(By.name("home")).click();
         browser.findElement(By.name("home")).clear();
-        browser.findElement(By.name("home")).sendKeys(homePhoneNumber);
+        browser.findElement(By.name("home")).sendKeys(contact.getHomePhoneNumber());
         browser.findElement(By.name("mobile")).click();
         browser.findElement(By.name("mobile")).clear();
-        browser.findElement(By.name("mobile")).sendKeys(mobilePhoneNumber);
+        browser.findElement(By.name("mobile")).sendKeys(contact.getMobilePhoneNumber());
         browser.findElement(By.name("work")).click();
         browser.findElement(By.name("work")).clear();
-        browser.findElement(By.name("work")).sendKeys(workPhoneNumber);
+        browser.findElement(By.name("work")).sendKeys(contact.getWorkPhoneNumber());
         browser.findElement(By.name("fax")).click();
         browser.findElement(By.name("fax")).clear();
-        browser.findElement(By.name("fax")).sendKeys(fax);
+        browser.findElement(By.name("fax")).sendKeys(contact.getFax());
         browser.findElement(By.name("email")).click();
         browser.findElement(By.name("email")).clear();
-        browser.findElement(By.name("email")).sendKeys(mainEmail);
+        browser.findElement(By.name("email")).sendKeys(contact.getMainEmail());
         browser.findElement(By.name("email2")).click();
         browser.findElement(By.name("email2")).clear();
-        browser.findElement(By.name("email2")).sendKeys(secondEmail);
+        browser.findElement(By.name("email2")).sendKeys(contact.getSecondEmail());
         browser.findElement(By.name("email3")).click();
         browser.findElement(By.name("email3")).clear();
-        browser.findElement(By.name("email3")).sendKeys(thirdEmail);
+        browser.findElement(By.name("email3")).sendKeys(contact.getThirdEmail());
         browser.findElement(By.name("homepage")).click();
         browser.findElement(By.name("homepage")).clear();
-        browser.findElement(By.name("homepage")).sendKeys(homePage);
+        browser.findElement(By.name("homepage")).sendKeys(contact.getHomePage());
 
         // Birth date
-        if (!browser.findElement(By.xpath("//div[@id='content']/form/select[1]//option[" + birthDay + "]")).isSelected()) {
-            browser.findElement(By.xpath("//div[@id='content']/form/select[1]//option[" + birthDay + "]")).click();
+        if (!browser.findElement(By.xpath("//div[@id='content']/form/select[1]//option[" + contact.getBirthDay() + "]")).isSelected()) {
+            browser.findElement(By.xpath("//div[@id='content']/form/select[1]//option[" + contact.getBirthDay() + "]")).click();
         }
-        if (!browser.findElement(By.xpath("//div[@id='content']/form/select[2]//option[" + birthMonth + "]")).isSelected()) {
-            browser.findElement(By.xpath("//div[@id='content']/form/select[2]//option[" + birthMonth + "]")).click();
+        if (!browser.findElement(By.xpath("//div[@id='content']/form/select[2]//option[" + contact.getBirthMonth() + "]")).isSelected()) {
+            browser.findElement(By.xpath("//div[@id='content']/form/select[2]//option[" + contact.getBirthMonth() + "]")).click();
         }
         browser.findElement(By.name("byear")).click();
         browser.findElement(By.name("byear")).clear();
-        browser.findElement(By.name("byear")).sendKeys(birthYear);
+        browser.findElement(By.name("byear")).sendKeys(contact.getBirthYear());
 
         // Anniversary date
-        if (!browser.findElement(By.xpath("//div[@id='content']/form/select[3]//option[" + anniverDay + "]")).isSelected()) {
-            browser.findElement(By.xpath("//div[@id='content']/form/select[3]//option[" + anniverDay + "]")).click();
+        if (!browser.findElement(By.xpath("//div[@id='content']/form/select[3]//option[" + contact.getAnniverDay() + "]")).isSelected()) {
+            browser.findElement(By.xpath("//div[@id='content']/form/select[3]//option[" + contact.getAnniverDay() + "]")).click();
         }
-        if (!browser.findElement(By.xpath("//div[@id='content']/form/select[4]//option[" + anniverMonth + "]")).isSelected()) {
-            browser.findElement(By.xpath("//div[@id='content']/form/select[4]//option[" + anniverMonth + "]")).click();
+        if (!browser.findElement(By.xpath("//div[@id='content']/form/select[4]//option[" + contact.getAnniverMonth() + "]")).isSelected()) {
+            browser.findElement(By.xpath("//div[@id='content']/form/select[4]//option[" + contact.getAnniverMonth() + "]")).click();
         }
         browser.findElement(By.name("ayear")).click();
         browser.findElement(By.name("ayear")).clear();
-        browser.findElement(By.name("ayear")).sendKeys(anniverYear);
+        browser.findElement(By.name("ayear")).sendKeys(contact.getAnniverYear());
 
         /*  group  selector */
 
         browser.findElement(By.name("address2")).click();
         browser.findElement(By.name("address2")).clear();
-        browser.findElement(By.name("address2")).sendKeys(secondAddress);
+        browser.findElement(By.name("address2")).sendKeys(contact.getSecondAddress());
         browser.findElement(By.name("phone2")).click();
         browser.findElement(By.name("phone2")).clear();
-        browser.findElement(By.name("phone2")).sendKeys(secondHomePhoneNumber);
+        browser.findElement(By.name("phone2")).sendKeys(contact.getSecondHomePhoneNumber());
         browser.findElement(By.name("notes")).click();
         browser.findElement(By.name("notes")).clear();
-        browser.findElement(By.name("notes")).sendKeys(notes);
+        browser.findElement(By.name("notes")).sendKeys(contact.getNotes());
     }
 
     private void submitContactCreation() {
---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,151 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+    private final String firstName;
+    private final String middleName;
+    private final String lastName;
+    private final String nickName;
+    private final String title;
+    private final String company;
+    private final String firstAddress;
+    private final String homePhoneNumber;
+    private final String mobilePhoneNumber;
+    private final String workPhoneNumber;
+    private final String fax;
+    private final String mainEmail;
+    private final String secondEmail;
+    private final String thirdEmail;
+    private final String homePage;
+    private final String birthDay;
+    private final String birthMonth;
+    private final String birthYear;
+    private final String anniverDay;
+    private final String anniverMonth;
+    private final String anniverYear;
+    private final String secondAddress;
+    private final String secondHomePhoneNumber;
+    private final String notes;
+
+    public ContactData(String firstName, String middleName, String lastName, String nickName, String title, String company, String firstAddress, String homePhoneNumber, String mobilePhoneNumber, String workPhoneNumber, String fax, String mainEmail, String secondEmail, String thirdEmail, String homePage, String birthDay, String birthMonth, String birthYear, String anniverDay, String anniverMonth, String anniverYear, String secondAddress, String secondHomePhoneNumber, String notes) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.nickName = nickName;
+        this.title = title;
+        this.company = company;
+        this.firstAddress = firstAddress;
+        this.homePhoneNumber = homePhoneNumber;
+        this.mobilePhoneNumber = mobilePhoneNumber;
+        this.workPhoneNumber = workPhoneNumber;
+        this.fax = fax;
+        this.mainEmail = mainEmail;
+        this.secondEmail = secondEmail;
+        this.thirdEmail = thirdEmail;
+        this.homePage = homePage;
+        this.birthDay = birthDay;
+        this.birthMonth = birthMonth;
+        this.birthYear = birthYear;
+        this.anniverDay = anniverDay;
+        this.anniverMonth = anniverMonth;
+        this.anniverYear = anniverYear;
+        this.secondAddress = secondAddress;
+        this.secondHomePhoneNumber = secondHomePhoneNumber;
+        this.notes = notes;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getFirstAddress() {
+        return firstAddress;
+    }
+
+    public String getHomePhoneNumber() {
+        return homePhoneNumber;
+    }
+
+    public String getMobilePhoneNumber() {
+        return mobilePhoneNumber;
+    }
+
+    public String getWorkPhoneNumber() {
+        return workPhoneNumber;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public String getMainEmail() {
+        return mainEmail;
+    }
+
+    public String getSecondEmail() {
+        return secondEmail;
+    }
+
+    public String getThirdEmail() {
+        return thirdEmail;
+    }
+
+    public String getHomePage() {
+        return homePage;
+    }
+
+    public String getBirthDay() {
+        return birthDay;
+    }
+
+    public String getBirthMonth() {
+        return birthMonth;
+    }
+
+    public String getBirthYear() {
+        return birthYear;
+    }
+
+    public String getAnniverDay() {
+        return anniverDay;
+    }
+
+    public String getAnniverMonth() {
+        return anniverMonth;
+    }
+
+    public String getAnniverYear() {
+        return anniverYear;
+    }
+
+    public String getSecondAddress() {
+        return secondAddress;
+    }
+
+    public String getSecondHomePhoneNumber() {
+        return secondHomePhoneNumber;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+}
