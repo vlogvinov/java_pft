8f86004e345bab8fe8d018d592ac95c259a30162
Vladimir Logvinov <volodymyr.logvinov@gmail.com>
2016-04-18 12:32:13 +0300

    Revert "added jcommander lib"
    This reverts commit 94b75ecb81822b96a4f46c1750b6101c2f5a922f.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/generators/GroupDataGenerator.java
@@ -1,8 +1,5 @@
 package ru.stqa.pft.addressbook.generators;
 
-import com.beust.jcommander.JCommander;
-import com.beust.jcommander.Parameter;
-import com.beust.jcommander.ParameterException;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.io.File;
@@ -13,30 +10,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GroupDataGenerator {
-    @Parameter(names = "-c", description = "Group count")
-    public int count;
-
-    @Parameter(names = "-f", description = "Target file")
-    public String file;
-
     public static void main(String[] args) throws IOException {
-        GroupDataGenerator generator = new GroupDataGenerator();
-        JCommander jCommander = new JCommander(generator);
-        try {
-            jCommander.parse(args);
-        }catch (ParameterException ex){
-            jCommander.usage();
-            return;
-        }
-        generator.run();
-    }
+        int count  = Integer.parseInt(args[0]); // groups quantity
+        File file = new File(args[1]); // file name
 
-    private void run() throws IOException {
+
         List<GroupData> groups = generateGroups(count);
-        save(groups, new File(file));
+        save(groups, file);
     }
 
-    private void save(List<GroupData> groups, File file) throws IOException {
+    private static void save(List<GroupData> groups, File file) throws IOException {
         Writer writer = new FileWriter(file);
         for(GroupData group : groups){
             writer.write(String.format("%s;%s;%s\n", group.getName(), group.getHeader(), group.getFooter()));
@@ -45,7 +28,7 @@ public class GroupDataGenerator {
     }
 
 
-    private List<GroupData> generateGroups(int count) {
+    private static List<GroupData> generateGroups(int count) {
         List<GroupData> groups = new ArrayList<GroupData>();
         for (int i = 0; i < count; i++){
             groups.add(new GroupData()
